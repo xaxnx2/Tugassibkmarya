f43a281dc80ab861fabb60ac25257660ac52df0f
xaxnx2 <aryayudistira10@gmail.com>
2023-04-28 03:32:21 +0700

    Added Tugas 6

---
+++ tugas6/tugas5/Models/Profiling.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Tugas4.Models
+{
+
+    [Table("tb_tr_profilings")]
+    public class Profiling
+    {
+        [Key, Column("employee_nik", TypeName = "char(5)")]
+        public string EmployeeNIK { get; set; }
+        [Column("education_id")]
+        public int EducationId { get; set; }
+
+        //cardinality
+        public Education Education { get; set; }
+        public Employee Employee { get; set; }
+    }
+}
---
+++ tugas6/tugas5/Models/University.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Tugas4.Models
+{
+    [Table("TB_M_Universities")]
+    public class University
+    {
+        [Key, Column("id")]
+        public int id { get; set; }
+        [Column("Name", TypeName = "varchar(100)")]
+        public string name { get; set; }
+
+        //cardinality
+        public ICollection<Education> Educations { get; set; }
+
+    }
+}
